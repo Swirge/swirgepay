ba624e04ef976fec415300e5eacc871475d8e0ee
MrJaycrypto <51384246+MrJaycrypto@users.noreply.github.com>
2020-09-17 11:54:02 +0100

    Changed to SwirgeElite

--- contracts/SwirgeElite.sol
+++ contracts/SwirgeElite.sol
@@ -6,26 +6,26 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./SwirgeToken.sol";
+import "./SushiToken.sol";
 
 
-interface IMigratorChef {
-    // Perform LP token migration from legacy UniswapV2 to SwirgePay.
+interface IMigratorElite {
+    // Perform LP token migration from legacy UniswapV2 to SushiSwap.
     // Take the current LP token address and return the new LP token address.
     // Migrator should have full access to the caller's LP token.
     // Return the new LP token address.
     //
     // XXX Migrator must have allowance access to UniswapV2 LP tokens.
-    // SwirgePay must mint EXACTLY the same amount of SwirgePay LP tokens or
+    // SushiSwap must mint EXACTLY the same amount of SushiSwap LP tokens or
     // else something bad will happen. Traditional UniswapV2 does not
     // do that so be careful!
     function migrate(IERC20 token) external returns (IERC20);
 }
 
-// SwirgeElite is the master of Swirge. He can make Swirge and he is a fair guy.
+// SwirgeElite is the master of Sushi. He can make Sushi and he is a fair guy.
 //
 // Note that it's ownable and the owner wields tremendous power. The ownership
-// will be transferred to a governance smart contract once SWG is sufficiently
+// will be transferred to a governance smart contract once SUSHI is sufficiently
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
@@ -38,13 +38,13 @@ contract SwirgeElite is Ownable {
         uint256 amount;     // How many LP tokens the user has provided.
         uint256 rewardDebt; // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of SWGs
+        // We do some fancy math here. Basically, any point in time, the amount of SUSHIs
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accSwirgePerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accSushiPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accSwirgePerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accSushiPerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -53,23 +53,23 @@ contract SwirgeElite is Ownable {
     // Info of each pool.
     struct PoolInfo {
         IERC20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. SWGs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that SWGs distribution occurs.
-        uint256 accSwirgePerShare; // Accumulated SWGs per share, times 1e12. See below.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. SUSHIs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that SUSHIs distribution occurs.
+        uint256 accSushiPerShare; // Accumulated SUSHIs per share, times 1e12. See below.
     }
 
-    // The SWG TOKEN!
-    SwirgeToken public swirge;
+    // The SUSHI TOKEN!
+    SushiToken public sushi;
     // Dev address.
     address public devaddr;
-    // Block number when bonus SWG period ends.
+    // Block number when bonus SUSHI period ends.
     uint256 public bonusEndBlock;
-    // SWG tokens created per block.
-    uint256 public swirgePerBlock;
-    // Bonus muliplier for early swirge makers.
+    // SUSHI tokens created per block.
+    uint256 public sushiPerBlock;
+    // Bonus muliplier for early sushi makers.
     uint256 public constant BONUS_MULTIPLIER = 10;
     // The migrator contract. It has a lot of power. Can only be set through governance (owner).
-    IMigratorChef public migrator;
+    IMigratorElite public migrator;
 
     // Info of each pool.
     PoolInfo[] public poolInfo;
@@ -77,7 +77,7 @@ contract SwirgeElite is Ownable {
     mapping (uint256 => mapping (address => UserInfo)) public userInfo;
     // Total allocation poitns. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when SWG mining starts.
+    // The block number when SUSHI mining starts.
     uint256 public startBlock;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -85,15 +85,15 @@ contract SwirgeElite is Ownable {
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
 
     constructor(
-        SwirgeToken _swirge,
+        SushiToken _sushi,
         address _devaddr,
-        uint256 _swirgePerBlock,
+        uint256 _sushiPerBlock,
         uint256 _startBlock,
         uint256 _bonusEndBlock
     ) public {
-        swirge = _swirge;
+        sushi = _sushi;
         devaddr = _devaddr;
-        swirgePerBlock = _swirgePerBlock;
+        sushiPerBlock = _sushiPerBlock;
         bonusEndBlock = _bonusEndBlock;
         startBlock = _startBlock;
     }
@@ -114,11 +114,11 @@ contract SwirgeElite is Ownable {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accSwirgePerShare: 0
+            accSushiPerShare: 0
         }));
     }
 
-    // Update the given pool's SWG allocation point. Can only be called by the owner.
+    // Update the given pool's SUSHI allocation point. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
         if (_withUpdate) {
             massUpdatePools();
@@ -128,7 +128,7 @@ contract SwirgeElite is Ownable {
     }
 
     // Set the migrator contract. Can only be called by the owner.
-    function setMigrator(IMigratorChef _migrator) public onlyOwner {
+    function setMigrator(IMigratorElite _migrator) public onlyOwner {
         migrator = _migrator;
     }
 
@@ -157,18 +157,18 @@ contract SwirgeElite is Ownable {
         }
     }
 
-    // View function to see pending SWGs on frontend.
-    function pendingSwirge(uint256 _pid, address _user) external view returns (uint256) {
+    // View function to see pending SUSHIs on frontend.
+    function pendingSushi(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accSwirgePerShare = pool.accSwirgePerShare;
+        uint256 accSushiPerShare = pool.accSushiPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 swirgeReward = multiplier.mul(swirgePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accSwirgePerShare = accSwirgePerShare.add(swirgeReward.mul(1e12).div(lpSupply));
+            uint256 sushiReward = multiplier.mul(sushiPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accSushiPerShare = accSushiPerShare.add(sushiReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accSwirgePerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accSushiPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -191,29 +191,29 @@ contract SwirgeElite is Ownable {
             return;
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 swirgeReward = multiplier.mul(swirgePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        swirge.mint(devaddr, swirgeReward.div(10));
-        swirge.mint(address(this), swirgeReward);
-        pool.accSwirgePerShare = pool.accSwirgePerShare.add(SwirgeReward.mul(1e12).div(lpSupply));
+        uint256 sushiReward = multiplier.mul(sushiPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        sushi.mint(devaddr, sushiReward.div(10));
+        sushi.mint(address(this), sushiReward);
+        pool.accSushiPerShare = pool.accSushiPerShare.add(sushiReward.mul(1e12).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to SwirgeElite for SWG allocation.
+    // Deposit LP tokens to SwirgeElite for SUSHI allocation.
     function deposit(uint256 _pid, uint256 _amount) public {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accSwirgePerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
-                safeSwirgeTransfer(msg.sender, pending);
+                safeSushiTransfer(msg.sender, pending);
             }
         }
         if(_amount > 0) {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accSwirgePerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -223,15 +223,15 @@ contract SwirgeElite is Ownable {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accSwirgePerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e12).sub(user.rewardDebt);
         if(pending > 0) {
-            safeSwirgeTransfer(msg.sender, pending);
+            safeSushiTransfer(msg.sender, pending);
         }
         if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accSwirgePerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -245,13 +245,13 @@ contract SwirgeElite is Ownable {
         user.rewardDebt = 0;
     }
 
-    // Safe swirge transfer function, just in case if rounding error causes pool to not have enough SWGs.
-    function safeSwirgeTransfer(address _to, uint256 _amount) internal {
-        uint256 swirgeBal = swirge.balanceOf(address(this));
-        if (_amount > swirgeBal) {
-            swirge.transfer(_to, swirgeBal);
+    // Safe sushi transfer function, just in case if rounding error causes pool to not have enough SUSHIs.
+    function safeSushiTransfer(address _to, uint256 _amount) internal {
+        uint256 sushiBal = sushi.balanceOf(address(this));
+        if (_amount > sushiBal) {
+            sushi.transfer(_to, sushiBal);
         } else {
-            swirge.transfer(_to, _amount);
+            sushi.transfer(_to, _amount);
         }
     }
 
